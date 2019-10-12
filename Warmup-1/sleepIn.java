bb5335dd0f2b598f6aa402f03e2a859f776764c0
Misha Ward <wardm5@uw.edu>
2019-10-12 15:49:01 -0700

    Create sleepIn.java

---
+++ Warmup-1/sleepIn.java
@@ -0,0 +1,8 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if (!weekday && !vacation)
+    return true;
+  else if (weekday && !vacation)
+    return false;
+  else 
+    return true;
+}

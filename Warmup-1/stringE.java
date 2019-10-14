d9b0a47c65823ae1bac8164bb2ccf3e1c66bc035
Misha Ward <wardm5@uw.edu>
2019-10-13 22:05:07 -0700

    Create stringE.java

---
+++ Warmup-1/stringE.java
@@ -0,0 +1,8 @@
+public boolean stringE(String str) {
+  int count = 0; 
+  for (int i = 0; i < str.length(); i++) {
+    if (str.charAt(i) == 'e')
+      count++;
+  }
+  return (count <= 3 && count >= 1);
+}

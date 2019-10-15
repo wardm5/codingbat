e825b8431dd5d5a0b0d2790890b286aada1625b4
Misha Ward <wardm5@uw.edu>
2019-10-14 23:51:42 -0700

    Create mapShare.java

---
+++ Map-1/mapShare.java
@@ -0,0 +1,7 @@
+public Map<String, String> mapShare(Map<String, String> map) {
+  if (map.containsKey("a"))
+    map.put("b", map.get("a"));
+  if (map.containsKey("c"))
+    map.remove("c");
+  return map;
+}

642a03350a89823974a5d55f260b420a084345a0
Misha Ward <wardm5@uw.edu>
2019-10-16 00:15:22 -0700

    Update arrayFront9.java

--- Warmup-2/arrayFront9.java
+++ Warmup-2/arrayFront9.java
@@ -1,11 +1,22 @@
+// public boolean arrayFront9(int[] nums) {
+//   int count = 0;
+//   for (int i = 0; i < nums.length; i++)  {
+//     if (count >= 4)
+//       return false;
+//     if (nums[i] == 9)
+//       return true;
+//     count++;
+//   }
+//   return false;
+// }
+
 public boolean arrayFront9(int[] nums) {
-  int count = 0;
-  for (int i = 0; i < nums.length; i++)  {
-    if (count >= 4)
-      return false;
+  int length = 4;
+  if (nums.length <= 4)
+    length = nums.length;
+  for (int i = 0; i < length; i++)  {
     if (nums[i] == 9)
       return true;
-    count++;
   }
   return false;
 }
